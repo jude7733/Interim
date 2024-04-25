ac18f982b0b362dddc05bd12a634afc87c4e6de9
judee7733 <judeputhussery360@gmail.com>
2024-04-25 12:35:46 +0530

    updated dependencies,  Created LoadingSkeleton component for reusing it in different places

---
+++ src/components/ui/LoadingSkeleton.tsx
@@ -0,0 +1,30 @@
+import { Skeleton } from "./skeleton";
+
+type LoadingSkeletonProps = { variant: string };
+export const LoadingSkeleton = ({ variant }: LoadingSkeletonProps) => {
+  return (
+    <>
+      {variant === "export" && (
+        <div className="flex flex-col items-center gap-8 w-auto px-[19px] py-7">
+          <div className="flex justify-start w-full">
+            <Skeleton className="h-8 w-60 rounded-xl" />
+          </div>
+          <div className="space-y-3 w-min">
+            <Skeleton className="h-6 w-72 rounded-xl" />
+            <Skeleton className="h-6 w-72 rounded-xl" />
+            <Skeleton className="h-6 w-72 rounded-xl" />
+            <Skeleton className="h-6 w-72 rounded-xl" />
+            <Skeleton className="h-6 w-72 rounded-xl" />
+            <Skeleton className="h-6 w-72 rounded-xl" />
+            <Skeleton className="h-6 w-72 rounded-xl" />
+            <Skeleton className="h-6 w-72 rounded-xl" />
+          </div>
+          <div className="flex w-80 justify-end gap-4">
+            <Skeleton className="h-7 w-24" />
+            <Skeleton className="h-7 w-24" />
+          </div>
+        </div>
+      )}
+    </>
+  );
+};
