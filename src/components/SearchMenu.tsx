db97621edd4ffcc64aa21192d14b0195b2467d0d
judee7733 <judeputhussery360@gmail.com>
2024-05-09 13:54:58 +0530

    replaced Command with Table

--- src/components/SearchMenu.tsx
+++ src/components/SearchMenu.tsx
@@ -1,17 +1,18 @@
-import { CheckIcon } from "lucide-react";
-import {
-  Command,
-  CommandEmpty,
-  CommandGroup,
-  CommandInput,
-  CommandItem,
-  CommandList,
-} from "./ui/command";
 import { useEffect, useState } from "react";
-import { Label } from "./ui/label";
 import { Head } from "./ui/head";
 import { searchPackage, SearchResult } from "@/app/shell";
 import { LoadingSkeleton } from "./ui/LoadingSkeleton";
+import { Input } from "./ui/input";
+import {
+  Table,
+  TableBody,
+  TableCaption,
+  TableCell,
+  TableHead,
+  TableHeader,
+  TableRow,
+} from "./ui/table";
+import { Search } from "lucide-react";
 
 const SearchMenu = () => {
   const [value, setValue] = useState("");
@@ -19,58 +20,88 @@ const SearchMenu = () => {
   const [loading, setLoading] = useState(false);
 
   useEffect(() => {
-    if (value.length > 2) {
-      setLoading(true);
-      searchPackage(value)
-        .then((data: SearchResult[] | null) => setResult(data))
-        .finally(() => setLoading(false));
-    }
+    let isMounted = true;
+    const fetchData = async () => {
+      if (value.length > 2) {
+        setLoading(true);
+        try {
+          const data = await searchPackage(value);
+          if (isMounted) {
+            setResult(data);
+            setLoading(false);
+          }
+        } catch (error) {
+          if (isMounted) {
+            console.error(error);
+            setLoading(false);
+          }
+        }
+      }
+    };
+    fetchData();
+
+    return () => {
+      isMounted = false;
+    };
   }, [value]);
 
   return (
-    <div className="flex flex-col items-start justify-start m-1 p-1 w-full h-full">
+    <div className="flex flex-col items-start justify-start m-1 p-1 w-full h-full gap-2">
       <Head title="Search" />
-      <div className="flex justify-center items-center w-full h-full p-10">
-        <Command className="rounded-lg border">
-          <CommandInput
-            placeholder="Search packages..."
-            value={value}
-            onInput={(e: React.ChangeEvent<HTMLInputElement>) =>
-              setValue(e.target.value)
-            }
-          />
-          <CommandList>
-            <CommandEmpty>
-              {value.length === 0
-                ? "Type something to search"
-                : !loading && "Not found"}
-            </CommandEmpty>
-            {loading && <LoadingSkeleton variant="search" />}
-            <CommandGroup heading="Results">
-              {result?.map((result) => (
-                <CommandItem
-                  key={result?.name}
-                  value={result?.name}
-                  className="flex flex-col items-start p-2 border-b mx-5"
-                >
-                  <div className="flex justify-start gap-14">
-                    <Label className="font-bold">{result?.name}</Label>
-                    <Label>{result?.version}</Label>
-                    <Label>{result?.architecture}</Label>
-                    <Label>{result?.source}</Label>
-                    {result?.installed && (
-                      <CheckIcon size={20} color="yellow" />
-                    )}
-                  </div>
-                  <div>
-                    <Label>{result?.description}</Label>
-                  </div>
-                </CommandItem>
-              ))}
-            </CommandGroup>
-          </CommandList>
-        </Command>
+      <div className="flex justify-center items-center w-1/3 mx-5 gap-2">
+        <Search />
+        <Input
+          placeholder="Search packages..."
+          value={value}
+          onInput={(e: React.ChangeEvent<HTMLInputElement>) =>
+            setValue(e.target.value)
+          }
+          className="border-2"
+        />
       </div>
+      <Table className="m-5 w-auto">
+        <TableCaption>
+          {value.length < 2
+            ? "Type any package name to search"
+            : !loading && result?.length + " results found"}
+        </TableCaption>
+        <TableHeader className="bg-accent">
+          <TableRow>
+            <TableHead>Name</TableHead>
+            <TableHead>Version</TableHead>
+            <TableHead>Architecture</TableHead>
+            <TableHead>Source</TableHead>
+            <TableHead>Installed</TableHead>
+            <TableHead>Description</TableHead>
+          </TableRow>
+        </TableHeader>
+        <TableBody>
+          {loading ? (
+            <>
+              <LoadingSkeleton variant="search" />
+              <LoadingSkeleton variant="search" />
+              <LoadingSkeleton variant="search" />
+              <LoadingSkeleton variant="search" />
+              <LoadingSkeleton variant="search" />
+              <LoadingSkeleton variant="search" />
+            </>
+          ) : (
+            result?.map((result) => (
+              <TableRow
+                key={result?.name}
+                className="hover:bg-secondary cursor-pointer"
+              >
+                <TableCell>{result?.name}</TableCell>
+                <TableCell>{result?.version}</TableCell>
+                <TableCell>{result?.architecture}</TableCell>
+                <TableCell>{result?.source}</TableCell>
+                <TableCell>{result?.installed ? "Yes" : "No"}</TableCell>
+                <TableCell>{result?.description}</TableCell>
+              </TableRow>
+            ))
+          )}
+        </TableBody>
+      </Table>
     </div>
   );
 };
